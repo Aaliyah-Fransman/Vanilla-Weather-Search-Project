c29b3937766249273a6e1d4ccccddaa6056e89c8
Aaliyah-Fransman <Aaliyah.Fransman@gmail.com>
2025-08-06 23:46:58 +0200

    Injected more weather data

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = temperature;
+  timeElement.innerHTML = formatDate(date);
 }
 
 function search(event) {
